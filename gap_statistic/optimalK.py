2dee8e1cc8f3f7fdcdde0671a55191e4965a04c0
Miles Granger <miles59923@gmail.com>
2017-08-12 15:12:17 +0200

    significant speed improvement, removal of scikit dep

--- gap_statistic/optimalK.py
+++ gap_statistic/optimalK.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 from multiprocessing import cpu_count
-from sklearn.cluster import KMeans
+from scipy.cluster.vq import kmeans2
 from joblib import Parallel, delayed
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
 class OptimalK:
@@ -14,35 +15,31 @@ class OptimalK:
         self.n_jobs = n_jobs if n_jobs >= 1 else cpu_count()  # type: int
 
     @staticmethod
-    def calculate_gap(data, nrefs, gap_index, k):
+    def calculate_gap(X, n_refs, gap_index, k):
 
         # Holder for reference dispersion results
-        refDisps = np.zeros(nrefs)
+        ref_dispersions = np.zeros(n_refs)
 
         # For n references, generate random sample and perform kmeans getting resulting dispersion of each loop
-        for i in range(nrefs):
+        for i in range(n_refs):
             # Create new random reference set
-            randomReference = np.random.random_sample(size=data.shape)
+            random_data = np.random.random_sample(size=X.shape)
 
             # Fit to it
-            km = KMeans(k)
-            km.fit(randomReference)
-
-            refDisp = km.inertia_
-            refDisps[i] = refDisp
+            centeroids, labels = kmeans2(random_data, k, iter=10, minit='points')
+            disp = np.sum(np.sum([np.abs(inst - centeroids[label]) ** 2 for inst, label in zip(random_data, labels)]))
+            ref_dispersions[i] = disp
 
         # Fit cluster to original data and create dispersion
-        km = KMeans(k)
-        km.fit(data)
-
-        origDisp = km.inertia_
+        centeroids, labels = kmeans2(X, k, iter=10, minit='points')
+        disp = np.sum(np.sum([np.abs(inst - centeroids[label]) ** 2 for inst, label in zip(X, labels)]))
 
         # Calculate gap statistic
-        gap = np.log(np.mean(refDisps)) - np.log(origDisp)
+        gap = np.log(np.mean(ref_dispersions)) - np.log(disp)
 
         return gap, gap_index, k
 
-    def __call__(self, data, nrefs=3, maxClusters=15):
+    def __call__(self, data, n_refs=3, maxClusters=15):
         """
         Calculates KMeans optimal K using Gap Statistic from Tibshirani, Walther, Hastie
         Params:
@@ -55,13 +52,29 @@ class OptimalK:
         df_results = pd.DataFrame({'clusterCount': [], 'gap': []})
 
         with Parallel(n_jobs=self.n_jobs) as parallel:
-
-            for gap_result, gap_index, k in parallel(delayed(self.calculate_gap)(data, nrefs, gap_index, k)
+            for gap_result, gap_index, k in parallel(delayed(self.calculate_gap)(data, n_refs, gap_index, k)
                                                      for gap_index, k in enumerate(range(1, maxClusters))):
-
                 # Assign this loop's gap statistic to gaps
                 gaps[gap_index] = gap_result
 
                 df_results = df_results.append({'clusterCount': k, 'gap': gap_result}, ignore_index=True)
+
+        # with ProcessPoolExecutor(max_workers=self.n_jobs) as executor:
+        #
+        #     jobs = {executor.submit(self.calculate_gap, data, nrefs, gap_index, k): k
+        #             for gap_index, k in enumerate(range(1, maxClusters))
+        #             }
+        #
+        #     for future in as_completed(jobs):
+        #         cluster_count = jobs[future]
+        #         try:
+        #             gap_result, gap_index, k = future.result()
+        #         except Exception as exc:
+        #             print('Got the following exception:\n{}'.format(exc))
+        #         else:
+        #             # Assign this loop's gap statistic to gaps
+        #             gaps[gap_index] = gap_result
+        #
+        #             df_results = df_results.append({'clusterCount': k, 'gap': gap_result}, ignore_index=True)
 
         return (gaps.argmax() + 1, df_results)  # Plus 1 because index of 0 means 1 cluster is optimal, index 2 = 3 clusters are optimal
