4b75f8d8f22f318bdf734355826c2af9166621a7
Miles Granger <miles59923@gmail.com>
2018-05-17 09:21:30 +0200

    Fix rust build

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,23 @@
 #![feature(proc_macro, specialization)]
 
-extern crate pyo3;
-
-extern crate ndarray;
-extern crate num;
+pub extern crate pyo3;
+pub extern crate ndarray;
+pub extern crate num;
 
 use pyo3::prelude::*;
 use pyo3::py::modinit as pymodinit;
 
+pub mod gap_statistic;
 
-#[pymodinit(rustgap)]
+
+#[pymodinit(gap_statistic)]
 fn init_mod(py: Python, m: &PyModule) -> PyResult<()> {
 
-    #[pyfn(m, "optimalK")]
-    fn optimalK_py(data: Vec<Vec<f64>>, clusters: Vec<u32>) -> PyResult(u32) {
-        Ok(optimalK())
+    #[pyfn(m, "gap_statistic")]
+    fn gap_statistic_py(data: Vec<Vec<f64>>, cluster_range: Vec<u32>) -> PyResult<u32> {
+       Ok(gap_statistic::optimalK())
     }
+
     Ok(())
 }
 
-fn optimalK() -> u32 {
-    2
-}
