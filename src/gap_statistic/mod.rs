6e6f181b8f50fe1d8fb5491c510a4f06778e502b
Miles Granger <miles59923@gmail.com>
2019-08-11 21:19:56 +0200

    Optimize Rust implementation

--- src/gap_statistic/mod.rs
+++ src/gap_statistic/mod.rs
@@ -1,56 +1,39 @@
-use ndarray::{Array1, Array2};
+use ndarray::{Array, Array1, Array2, ArrayView1, ArrayView2};
+use ndarray_parallel::prelude::*;
 use ndarray_rand::RandomExt;
 use num_traits::pow::Pow;
-use rand::distributions::Range;
+use rand::distributions::Uniform;
 use statrs::statistics::Statistics;
 use std::collections::HashMap;
 use std::iter::FromIterator;
 
 use kmeans::{Centroid, KMeans};
 
-// Kmeans Entry point
-pub fn kmeans<'a>(
-    data: &'a Array2<f64>,
-    k: u32,
-    max_iter: u32,
-    iter: u32,
-) -> (Vec<Centroid>, Vec<u32>) {
-    let mut kmeans = KMeans::new(k, 0.001, max_iter, iter);
-    kmeans.fit(&data);
-    let labels = kmeans.predict(&data);
-    (
-        kmeans
-            .centroids
-            .expect("No centroids inside of KMeans model!")
-            .clone(),
-        labels.clone(),
-    )
-}
-
 pub fn convert_2d_vec_to_array(data: Vec<Vec<f64>>) -> Array2<f64> {
     // Convert vector to Array
     let shape = (data.len(), data[0].len());
     //let mut array = Array2::zeros(shape);
-    let data = Array1::from_iter(data.iter().flat_map(|v| v.clone())) // TODO: Do better than clone...
+    let data = Array1::from_iter(data.into_iter().flat_map(|v| v))
         .into_shape(shape)
         .expect("Failed to reshape!");
     data
 }
 
-pub fn optimal_k(data: Vec<Vec<f64>>, cluster_range: Vec<u32>, iter: u32) -> Vec<GapCalcResult> {
+pub fn optimal_k(
+    data: &ArrayView2<f64>,
+    cluster_range: ArrayView1<i64>,
+    iter: u32,
+) -> Vec<GapCalcResult> {
     /*
         Given 2d data and a cluster range, return a vector of tuples
         where the first element represents n_clusters, gap value, and last is the ref disp std.
         (Higher the gap value, the better!)
     */
-    let data = convert_2d_vec_to_array(data);
-    let cluster_range = Array1::from_vec(cluster_range);
 
     // Get gap values for each cluster in range.
-
     let gap_values = cluster_range
-        .into_iter()
-        .map(|n_clusters| calculate_gap(&data, n_clusters.clone(), iter))
+        .into_par_iter()
+        .map(|n_clusters| calculate_gap(data, *n_clusters as u32, iter))
         .collect::<Vec<GapCalcResult>>();
 
     gap_values
@@ -67,26 +50,29 @@ pub struct GapCalcResult {
 }
 
 // Calculate the gap value
-fn calculate_gap(data: &Array2<f64>, n_clusters: u32, iter: u32) -> GapCalcResult {
+fn calculate_gap(data: &ArrayView2<f64>, n_clusters: u32, iter: u32) -> GapCalcResult {
     let n_refs = 5; // TODO: Add this as parameter
-    let mut ref_dispersions = Array1::zeros((n_refs,));
 
-    // For each reference check, run k-means on random data resembling shape of data.
-    for i in 0..n_refs {
-        // Generate some random data for this round
-        let random_data = Array2::random(data.dim(), Range::new(-1_f64, 1_f64));
+    let mut km = KMeans::new(n_clusters, 0.00005, 20, iter);
 
-        // Get centroids from data, each centroid contains .point() and .label()
-        let (centroids, labels) = kmeans(&random_data, n_clusters, 5, iter);
-        ref_dispersions[i] = calculate_dispersion(&random_data, labels, centroids);
-    }
+    let ref_dispersions = (0..n_refs)
+        .map(|_| {
+            let random_data = Array::random(data.dim(), Uniform::new(0., 1.));
+
+            // Get centroids from data, each centroid contains .point() and .label()
+            km.fit(&random_data.view());
+            let labels = km.predict(&random_data.view());
+            let centroids = km.centroids.as_ref().unwrap();
+            calculate_dispersion(&random_data.view(), labels, centroids)
+        })
+        .collect::<Vec<f64>>();
 
     // Do calculations for the actual data
-    let (centroids, labels) = kmeans(&data, n_clusters, 5, iter);
+    km.fit(&data);
+    let labels = km.predict(&data);
+    let centroids = km.centroids.unwrap();
 
-    let ref_dispersions = ref_dispersions.to_vec();
-
-    let dispersion = calculate_dispersion(&data, labels, centroids);
+    let dispersion = calculate_dispersion(data, labels, &centroids);
     let ref_log_dispersion = ref_dispersions.iter().map(|v| v.log2()).mean();
     let log_dispersion = dispersion.log2();
     let gap_value = ref_log_dispersion - log_dispersion;
@@ -118,7 +104,11 @@ fn calculate_gap(data: &Array2<f64>, n_clusters: u32, iter: u32) -> GapCalcResul
 
 // Calculate the dispersion
 // TODO: Implement this
-fn calculate_dispersion(data: &Array2<f64>, labels: Vec<u32>, centroids: Vec<Centroid>) -> f64 {
+fn calculate_dispersion(
+    data: &ArrayView2<f64>,
+    labels: Vec<u32>,
+    centroids: &Vec<Centroid>,
+) -> f64 {
     // Place centroids in hashmap for looking up
     let centroid_lookup = HashMap::<u32, Array1<f64>>::from_iter(
         centroids
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,71 @@
 #![feature(specialization, test)]
 
 #[macro_use]
-pub extern crate pyo3;
-#[macro_use]
 pub extern crate ndarray;
 pub extern crate ndarray_parallel;
 pub extern crate ndarray_rand;
 pub extern crate num_traits;
+pub extern crate numpy;
+pub extern crate pyo3;
 pub extern crate rand;
 pub extern crate rayon;
 pub extern crate statrs;
 extern crate test;
 
-use pyo3::prelude::*;
+use kmeans::KMeans;
+use numpy::{PyArray1, PyArray2};
+use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
 
 pub mod gap_statistic;
 pub mod kmeans;
 #[cfg(test)]
 mod tests;
 
-#[pyfunction]
-fn kmeans(data: Vec<Vec<f64>>, k: u32, max_iter: u32, iter: u32) -> PyResult<Vec<u32>> {
-    let data = gap_statistic::convert_2d_vec_to_array(data);
-    let (_centroids, labels) = gap_statistic::kmeans(&data, k, max_iter, iter);
-    Ok(labels)
-}
+#[pymodule]
+fn gapstat(_py: Python, m: &PyModule) -> Result<(), pyo3::PyErr> {
+    #[pyfn(m, "kmeans")]
+    fn kmeans(data: Vec<Vec<f64>>, k: u32, max_iter: u32, iter: u32) -> PyResult<Vec<u32>> {
+        let data = gap_statistic::convert_2d_vec_to_array(data);
+        let km = KMeans::new(k, 0.00005, max_iter, iter);
+        let labels = km.predict(&data.view());
+        Ok(labels)
+    }
 
-#[pyfunction]
-fn optimal_k(
-    data: Vec<Vec<f64>>,
-    cluster_range: Vec<u32>,
-    iter: Option<u32>,
-) -> PyResult<Vec<(u32, f64, f64, f64, f64, f64, f64)>> {
-    let gapcalcs = if let Some(iterations) = iter {
-        gap_statistic::optimal_k(data, cluster_range, iterations)
-    } else {
-        gap_statistic::optimal_k(data, cluster_range, 10)
-    };
-    let result = gapcalcs
-        .into_iter()
-        .map(|gapcalc| {
-            (
-                gapcalc.n_clusters,
-                gapcalc.gap_value,
-                gapcalc.ref_dispersion_std,
-                gapcalc.sdk,
-                gapcalc.sk,
-                gapcalc.gap_star,
-                gapcalc.sk_star,
-            )
-        })
-        .collect();
-    Ok(result)
-}
+    #[pyfn(m, "optimal_k")]
+    fn optimal_k(
+        py: Python,
+        data: &PyArray2<f64>,
+        cluster_range: &PyArray1<i64>,
+        iter: Option<i64>,
+    ) -> PyResult<Vec<(i64, f64, f64, f64, f64, f64, f64)>> {
+        let x = data.as_array();
+        let cr = cluster_range.as_array();
 
-#[pymodinit]
-fn gapstat(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_function!(kmeans))?;
-    m.add_function(wrap_function!(optimal_k))?;
+        let gapcalcs = py.allow_threads(move || {
+            if let Some(iterations) = iter {
+                gap_statistic::optimal_k(&x.view(), cr, iterations as u32)
+            } else {
+                gap_statistic::optimal_k(&x.view(), cr, 10)
+            }
+        });
+
+        let results = gapcalcs
+            .into_iter()
+            .map(|gapcalc| {
+                (
+                    gapcalc.n_clusters as i64,
+                    gapcalc.gap_value,
+                    gapcalc.ref_dispersion_std,
+                    gapcalc.sdk,
+                    gapcalc.sk,
+                    gapcalc.gap_star,
+                    gapcalc.sk_star,
+                )
+            })
+            .collect();
+
+        Ok(results)
+    }
+
     Ok(())
 }
